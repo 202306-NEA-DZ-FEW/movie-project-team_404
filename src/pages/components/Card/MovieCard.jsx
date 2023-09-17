a45197cf1c75b97f01e5a208332b1711bf744766
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-17 18:33:57 +0100

    fix(moviecard): updated the responsiveness of Movie Card

--- src/pages/components/Card/MovieCard.jsx
+++ src/pages/components/Card/MovieCard.jsx
@@ -1,14 +1,19 @@
 const MovieCard = ({ title, poster_path }) => {
   return (
-    <div className="card shadow-xl h-[100%] border hover:scale-105 transition-transform duration-300 hover:border-blue-500 transition-border duration-300">
+    <div
+      className="card mx-auto shadow-xl h-[100%] w-[70vw] sm:w-[25vw] md:w-[20vw] xl:w-[15vw] 2xl:w-[15vw]
+      border hover:scale-105 transition-transform duration-300 hover:border-blue-500 transition-border duration-300"
+    >
       <figure>
         <img
           src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
           alt={`${title} poster`}
         />
       </figure>
-      <div className="card-body p-4 grid place-content-center text-center text-white">
-        <h2 className="card-title ">{title}</h2>
+      <div className="card-body p-4 grid place-content-center text-center">
+        <h2 className="card-title sm:text-[2vw] md:text-[1.8vw] lg:text-[1.5vw] xl:text-[1.2vw] 2xl:text-[1.2vw]">
+          {title}
+        </h2>
       </div>
     </div>
   )
