7fb63e152e14a7fedaf54bb6b5c98e45130a4412
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-17 18:31:30 +0100

    chore(next.config.js): added fetch images domains

--- next.config.js
+++ next.config.js
@@ -12,13 +12,11 @@ const nextConfig = {
         pathname: "/**/**/**",
       },
     ],
+    domains: ["image.tmdb.org", "themoviedb.org"],
   },
   reactStrictMode: true,
   env: {
     API_KEY: process.env.API_KEY,
-  },
-  images: {
-    domains: ["image.tmdb.org", "themoviedb.org"],
   },
 }
 
