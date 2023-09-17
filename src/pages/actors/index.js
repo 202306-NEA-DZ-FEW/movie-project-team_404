c6e9e87e325321f7d23250cec13eb555515a102f
hamid <ahmidatabdelhamid@gmail.com>
2023-09-17 16:16:07 +0100

    fix: fix dynamic routing the single actor page

--- src/pages/actors/index.js
+++ src/pages/actors/index.js
@@ -1,12 +1,15 @@
 import { moviesFetcher } from "@/utils/api"
 import ActorsCard from "@/components/ActorsCard"
+import Link from "next/link"
 
 export default function Actors({ actors }) {
   return (
     <div className="h- 500 grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5  gap-10 mx-auto justify-items-center">
       {actors.results.map((actor) => (
         <div className="w-4/5 h-4/5" key={actor.id}>
-          <ActorsCard {...actor} />
+          <Link href={`actors/${actor.id}`}>
+            <ActorsCard {...actor} />
+          </Link>
         </div>
       ))}
     </div>
