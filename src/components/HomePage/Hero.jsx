aadf5d2b9073f65ddce83238e07f02e578626e96
Hocine Benouddane <hocine20992@gmail.com>
2023-09-18 16:33:58 +0100

    fix(hero.jsx): fix broken link that leads to single movie page
    fix #14

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = ({ movies }) => {
           </p>
 
           <Link
-            href={`movies/${movies[currentIndex].id}`}
+            href={`movies/movie/${movies[currentIndex].id}`}
             className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 my-22 rounded-full transition duration-300 ease-in-out"
           >
             Watch The Trailer
