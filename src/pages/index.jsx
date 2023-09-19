b5f61b896201c61d5b8d548320a017d2791c5681
Hocine Benouddane <hocine20992@gmail.com>
2023-09-19 13:18:33 +0100

    feat(hero.jsx): add slide effect to the carousel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 function HomePage({ latestMovies }) {
   return (
-    <main className="font-mono ">
+    <main>
       <Hero movies={latestMovies.results} />
       <div className="w-4/5 sm:w-full  sm:px-20  grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 mx-auto justify-center p-8 ">
         {latestMovies.results.map((movie) => {
